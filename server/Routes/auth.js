22e120065c6f49221cdc1e005a11110094bb1202
Rohit-saxena125 <rohitsaxena125@gmail.com>
2023-04-13 11:54:21 +0530

    done

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./Models/user');
+const User = require('../Models/user');
 router.post('/register', async (req, res) => {
     try{
         const {email, username, password ,role} = req.body;
-        const user = new User({email, username,role});
-        const registeredUser = await User.register(user, password);
+        const user = new User({email, username,role, password});
+        const registeredUser = await user.save();
         res.send(registeredUser);
     }catch(e){
         res.send(e.message);
     }
 });
-
+router.post('/login', async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        
+    }
+    catch(e){
+        res.send(e.message);
+    }
+});
 module.exports = router;
--- server/Server.js
+++ server/Server.js
@@ -1,9 +1,33 @@
+if(process.env.NODE_ENV != "production"){
+
+    require("dotenv").config({ path: "./.env"})
+  }
 const express = require('express');
+const cors = require('cors');
+const path = require("path")
 const app = express();
 const port = process.env.PORT || 5000;
+const methodOverride = require("method-override");
+const session = require("express-session")
+const flash = require("connect-flash");
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Connected to DB');
 }).catch(err => {
     console.log('ERROR:', err.message);
+});
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+const user = require("./Models/user");
+const auth = require('./Routes/auth');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(methodOverride("_method"));
+app.use(auth);
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
