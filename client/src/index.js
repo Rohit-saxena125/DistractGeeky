f54683dc045b0443a5d5bcd36664b26873993371
Rohit-saxena125 <rohitsaxena125@gmail.com>
2023-04-23 14:32:56 +0530

    done

--- client/src/index.js
+++ client/src/index.js
@@ -6,17 +6,31 @@ import App from './App';
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import History from '../src/components/history';
+import { Box } from '@mui/material';
+
+import { ChannelDetail, VideoDetail, SearchFeed, Feed } from './components';
 
 ReactDOM.render(
 
   <Router>
+    <Box sx={{ backgroundColor: '#000' }}>
     <Routes>
       <Route path='/' element={<App />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/history' element={<History />} />
+      
+      
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+    
+    
     </Routes>
+    </Box>
   </Router>,
 
   document.getElementById('root')
 );
+
